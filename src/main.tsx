2e572b523cf1030076f285f88f1a28b505270579
Giacomo Ferretti <giacomo.ferretti.00@gmail.com>
2022-05-16 18:06:47 +0200

    init: using https://github.com/giacomoferretti/vite-starter

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+import App from "@/App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          {/* <Route index element={<Home />} />
+        <Route path="teams" element={<Teams />}>
+          <Route path=":teamId" element={<Team />} />
+          <Route path="new" element={<NewTeamForm />} />
+          <Route index element={<LeagueStandings />} />
+        </Route> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
