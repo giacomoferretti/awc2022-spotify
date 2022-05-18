2a14a05d7d0cd7475dc08dca514c1c231f48ee25
Giacomo Ferretti <giacomo.ferretti.00@gmail.com>
2022-05-18 12:43:07 +0200

    fix: React.StrictMode intentionally render components two times
    More info here: https://stackoverflow.com/questions/61254372/

--- src/main.tsx
+++ src/main.tsx
@@ -7,18 +7,11 @@ import App from "@/App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          {/* <Route index element={<Home />} />
-        <Route path="teams" element={<Teams />}>
-          <Route path=":teamId" element={<Team />} />
-          <Route path="new" element={<NewTeamForm />} />
-          <Route index element={<LeagueStandings />} />
-        </Route> */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+    </Routes>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
