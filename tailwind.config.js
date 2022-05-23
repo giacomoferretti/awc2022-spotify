c53696370cea11aa351cd306dbdb5f05ff9bd961
Giacomo Ferretti <giacomo.ferretti.00@gmail.com>
2022-05-23 23:36:00 +0200

    feat: added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        "spotify-base": "#121212",
+        "spotify-elevated-base": "#242424",
+        "spotify-accent-base": "#1ed760",
+        "spotify-accent-highlight": "#1fdf64",
+        "spotify-error": "#e91429",
+      },
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
