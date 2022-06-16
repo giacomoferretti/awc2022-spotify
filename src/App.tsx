a1a9aa67d2f6270aec10af3b8bc77dd2f0642c89
Giacomo Ferretti <giacomo.ferretti.00@gmail.com>
2022-06-17 00:06:16 +0200

    feat: add debug router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { BrowserHistory, createBrowserHistory } from "history";
+import { useEffect, useLayoutEffect, useState } from "react";
 import {
   BrowserRouter,
   Navigate,
   Outlet,
   Route,
+  Router,
   Routes,
   createSearchParams,
   useLocation,
@@ -16,8 +18,7 @@ import { AuthDebug } from "@/pages/Debug/AuthDebug";
 import { Debug, DebugAbsoluteNav } from "@/pages/Debug/Debug";
 import { SpotifyDebug } from "@/pages/Debug/SpotifyDebug";
 import { TestForm } from "@/pages/Debug/TestForm";
-
-import { UserProfile } from "./pages/UserProfile";
+import { UserProfile, UserRedirect } from "@/pages/UserProfile";
 
 const RequireAuth = ({ children }: { children: React.ReactNode }) => {
   const location = useLocation();
@@ -71,9 +72,44 @@ const Logout = () => {
   return <Navigate to="/" replace />;
 };
 
-export const App = () => {
+const history = createBrowserHistory();
+
+const CustomRouter = ({
+  history,
+  children,
+}: {
+  history: BrowserHistory;
+  children?: React.ReactNode;
+}) => {
+  const [state, setState] = useState({
+    action: history.action,
+    location: history.location,
+  });
+
+  useLayoutEffect(() => history.listen(setState), [history]);
+
   return (
-    <BrowserRouter>
+    <Router
+      location={state.location}
+      navigationType={state.action}
+      navigator={history}>
+      {children}
+    </Router>
+  );
+};
+
+export const App = () => {
+  useEffect(() => {
+    const unlisten = history.listen((update) => {
+      console.log(update.location);
+      console.log(update.action);
+    });
+
+    return unlisten;
+  }, []);
+
+  return (
+    <CustomRouter history={history}>
       {/* <Helmet>
         <script
           async
@@ -99,6 +135,7 @@ export const App = () => {
 
         <Route element={<RequireAuthWrapper />}>
           <Route path="dashboard" element={<Dashboard />} />
+          <Route path="user" element={<UserRedirect />} />
           <Route path="user/:username" element={<UserProfile />} />
         </Route>
 
@@ -110,6 +147,6 @@ export const App = () => {
 
         <Route path="*" element={<NoMatch />} />
       </Routes>
-    </BrowserRouter>
+    </CustomRouter>
   );
 };
