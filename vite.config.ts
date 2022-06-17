f59b0040b2e27a748fcc1ba1c97bf31f7614aeb8
Giacomo Ferretti <giacomo.ferretti.00@gmail.com>
2022-06-17 09:44:33 +0200

    feat: add minify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
@@ -7,5 +8,11 @@ export default defineConfig({
   server: {
     host: true,
   },
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ],
 });
