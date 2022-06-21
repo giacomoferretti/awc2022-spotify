b641a5a5efea3c937b3820d76f3766cc9c214116
Giacomo Ferretti <giacomo.ferretti.00@gmail.com>
2022-06-21 10:33:22 +0200

    refactor: change menu DebugNav

--- src/pages/Debug/Debug.tsx
+++ src/pages/Debug/Debug.tsx
@@ -1,72 +1,64 @@
-import { Link, Outlet } from "react-router-dom";
+import classNames from "classnames";
+import { Link, LinkProps, Outlet } from "react-router-dom";
 
 import { usePlaylists, useUsers } from "@/context";
+
+const LinkButton = (props: LinkProps) => {
+  return (
+    <Link
+      {...props}
+      className={classNames(
+        "rounded bg-gray-700 py-1 px-2 font-mono",
+        props.className
+      )}>
+      {props.to.toString()}
+    </Link>
+  );
+};
 
 export const DebugAbsoluteNav = () => {
   const { clearAll: clearAllUsers } = useUsers();
   const { clearAll: clearAllPlaylists, generatePlaylist } = usePlaylists();
 
-  const clearUsersOnClick = () => {
-    clearAllUsers();
-  };
-
-  const clearPlaylistOnClick = () => {
-    clearAllPlaylists();
-  };
-
-  const createRandomPlaylist = () => {
-    generatePlaylist();
-  };
+  const clearUsersOnClick = () => clearAllUsers();
+  const clearPlaylistOnClick = () => clearAllPlaylists();
+  const createRandomPlaylist = () => generatePlaylist();
 
   return (
-    <div className="fixed bottom-0 flex flex-wrap items-center gap-2 p-2 text-xs opacity-50">
-      <h2>Navigation: </h2>
-      <Link className="rounded bg-gray-700 py-1 px-2 font-mono" to="/">
-        Home
-      </Link>
-      <Link className="rounded bg-gray-700 py-1 px-2 font-mono" to="/dashboard">
-        Dashboard
-      </Link>
-      <Link className="rounded bg-gray-700 py-1 px-2 font-mono" to="/login">
-        Login
-      </Link>
-      <Link className="rounded bg-gray-700 py-1 px-2 font-mono" to="/signup">
-        Signup
-      </Link>
-      <Link className="rounded bg-gray-700 py-1 px-2 font-mono" to="/logout">
-        Logout
-      </Link>
-      <Link className="rounded bg-gray-700 py-1 px-2 font-mono" to="/404">
-        404
-      </Link>
-      <Link
-        className="rounded bg-gray-700 py-1 px-2 font-mono"
-        to="/debug/spotify">
-        (DEBUG) Spotify
-      </Link>
-      <Link
-        className="rounded bg-gray-700 py-1 px-2 font-mono"
-        to="/debug/auth">
-        (DEBUG) Auth
-      </Link>
-      <button
-        className="rounded bg-gray-700 py-1 px-2 font-mono"
-        type="button"
-        onClick={clearUsersOnClick}>
-        Clear USERS
-      </button>
-      <button
-        className="rounded bg-gray-700 py-1 px-2 font-mono"
-        type="button"
-        onClick={clearPlaylistOnClick}>
-        Clear PLAYLISTS
-      </button>
-      <button
-        className="rounded bg-gray-700 py-1 px-2 font-mono"
-        type="button"
-        onClick={createRandomPlaylist}>
-        Create PLAYLIST
-      </button>
+    <div className="fixed bottom-0 max-w-lg p-2 text-xs opacity-50">
+      <div className="flex flex-col items-start gap-2">
+        <LinkButton to="/" />
+        <LinkButton to="/404" />
+        <LinkButton className="bg-red-900" to="/login" />
+        <LinkButton className="bg-red-900" to="/signup" />
+        <LinkButton className="bg-red-900" to="/logout" />
+        <LinkButton className="bg-green-900" to="/dashboard" />
+        <LinkButton className="bg-green-900" to="/user" />
+        <LinkButton className="bg-green-900" to="/playlist" />
+        <LinkButton className="bg-green-900" to="/playlist/new" />
+        <LinkButton className="bg-green-900" to="/search" />
+        <LinkButton className="bg-blue-900" to="/debug/spotify" />
+        <LinkButton className="bg-blue-900" to="/debug/auth" />
+
+        <button
+          className="rounded bg-gray-700 py-1 px-2 font-mono"
+          type="button"
+          onClick={clearUsersOnClick}>
+          [users] clearAll
+        </button>
+        <button
+          className="rounded bg-gray-700 py-1 px-2 font-mono"
+          type="button"
+          onClick={clearPlaylistOnClick}>
+          [playlists] clearAll
+        </button>
+        <button
+          className="rounded bg-gray-700 py-1 px-2 font-mono"
+          type="button"
+          onClick={createRandomPlaylist}>
+          [playlists] generatePlaylist
+        </button>
+      </div>
     </div>
   );
 };
